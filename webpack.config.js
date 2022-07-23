d034d240377045cacb3b78fce511bc84c6e73580
ibrahim-kocyigit <ibrahimkocyigit@gmail.com>
2022-07-23 09:21:56 +0300

    resolves favicon issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackFavicons = require("webpack-favicons");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    bundle: path.resolve(__dirname, 'src/js/controller.js'),
+    bundle: path.resolve(__dirname, "src/js/controller.js"),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name][contenthash].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
     clean: true,
-    assetModuleFilename: '[name][ext]',
+    assetModuleFilename: "./img/[name][ext]",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, "dist"),
     },
     port: 3000,
     open: true,
@@ -27,28 +28,37 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
+      filename: "index.html",
+      template: "index.html",
+    }),
+    new WebpackFavicons({
+      src: "src/img/favicon.png",
+      path: "img",
+      background: "#000",
+      theme_color: "#000",
+      icons: {
+        favicons: true,
+      },
     }),
   ],
 };
